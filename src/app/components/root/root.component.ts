f863fd8b3666d11d515d94cea9246f3eabeed54e
Binh Tran <tranthaibinh111@yahoo.com>
2019-08-23 08:52:54 +0700

    Add button contact at bottom screen

--- src/app/components/root/root.component.ts
+++ src/app/components/root/root.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RootService } from '../../shared/services/root.service';
+import { StoreService } from 'src/app/shared/services/store.service';
 
 @Component({
-    selector: 'app-main',
-    templateUrl: './root.component.html',
-    styleUrls: ['./root.component.scss']
+  selector: 'app-main',
+  templateUrl: './root.component.html',
+  styleUrls: ['./root.component.scss']
 })
 export class RootComponent {
-    headerLayout: string;
+  headerLayout: string;
 
-    constructor(
-        public root: RootService,
-        public router: Router,
-        public route: ActivatedRoute
-    ) {
-        this.route.data.subscribe(data => this.headerLayout = data.headerLayout);
-    }
+  constructor(
+    public root: RootService,
+    public router: Router,
+    public route: ActivatedRoute,
+    public store: StoreService
+  ) {
+    this.route.data.subscribe(data => this.headerLayout = data.headerLayout);
+  }
 }
