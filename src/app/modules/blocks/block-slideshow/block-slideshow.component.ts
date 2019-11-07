2cc9af10f99e2b06ca2f3a6741f572fde7f40824
Binh Tran <tranthaibinh111@yahoo.com>
2019-11-08 03:15:08 +0700

    Fix bug: IOS don't show image *.Webp

--- src/app/modules/blocks/block-slideshow/block-slideshow.component.ts
+++ src/app/modules/blocks/block-slideshow/block-slideshow.component.ts
@@ -25,23 +25,23 @@ export class BlockSlideshowComponent {
         {
             title: 'Yến Vàng Miền Nam',
             text: 'Chăm sóc cho sức khỏe là chăm sóc tương lai.',
-            image_classic: 'assets/images/slides/slide-1.webp',
-            image_full: 'assets/images/slides/slide-1-full.webp',
-            image_mobile: 'assets/images/slides/slide-1-mobile.webp'
+            image_classic: 'assets/images/slides/slide-1.jpg',
+            image_full: 'assets/images/slides/slide-1-full.jpg',
+            image_mobile: 'assets/images/slides/slide-1-mobile.jpg'
         },
         {
             title: 'Mr. Thắng<br>094 280 1401 (zalo)',
             text: 'Hân hạnh được tư vấn cho bạn.',
-            image_classic: 'assets/images/slides/slide-2.webp',
-            image_full: 'assets/images/slides/slide-2-full.webp',
-            image_mobile: 'assets/images/slides/slide-2-mobile.webp'
+            image_classic: 'assets/images/slides/slide-2.jpg',
+            image_full: 'assets/images/slides/slide-2-full.jpg',
+            image_mobile: 'assets/images/slides/slide-2-mobile.jpg'
         },
         {
             title: 'Ms. Ngân<br>039 573 5218 (zalo)',
             text: 'Sức khỏe của bạn là sự thành công của chúng tôi',
-            image_classic: 'assets/images/slides/slide-3.webp',
-            image_full: 'assets/images/slides/slide-3-full.webp',
-            image_mobile: 'assets/images/slides/slide-3-mobile.webp'
+            image_classic: 'assets/images/slides/slide-3.jpg',
+            image_full: 'assets/images/slides/slide-3-full.jpg',
+            image_mobile: 'assets/images/slides/slide-3-mobile.jpg'
         }
     ];
 
